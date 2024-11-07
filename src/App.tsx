7a9fdc7e1450e9f85a1c6a9cec762ee481a6ffb5
sebassg <sebastiandaks@gmail.com>
2024-11-07 17:06:47 -0500

    agrego react con tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="text-center bg-blue-500 text-white p-6">
+      <h1 className="text-4xl font-bold">Hello, Tailwind with TypeScript!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
