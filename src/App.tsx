adb38ac1e7c82dc3cfa2fcefe5500e6ec01a7621
insector07 <juanfergomezrivas@gmail.com>
2024-11-07 20:44:27 -0500

    sobre nosotros chambeado

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css'
+import './index.css';
 import PagInicio from './componentes/PagInicio';
 import Servicios from './componentes/servicios';
+import Nosotros from './componentes/Nosotros'; 
+
+
 function App() {
-  return (
+  return ( <>
+  
     <Router>
       <Routes>
         <Route path="/inicio" element={<PagInicio />} />
-        <Route path="/servicios" element={<Servicios/>} />
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/nosotros" element={<Nosotros />} /> {}
       </Routes>
     </Router>
+  </>
   );
 }
 
